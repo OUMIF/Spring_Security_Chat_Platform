22fbcfadaf3b2252a0cacaa99bda3d7ea4b837e4
EL Mehdi El Adnani <eladnanimehdi@gmail.com>
2025-09-08 23:49:51 +0100

    j'ai ajouter addEmployee interface plus connecter avec le backend

--- public/ems-frontend/src/services/EmployeeService.js
+++ public/ems-frontend/src/services/EmployeeService.js
@@ -5,3 +5,5 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 
 export const listEmployees = () => axios.get(API_URL);
+
+export const createEmployee = (employee) => axios.post(API_URL, employee);
